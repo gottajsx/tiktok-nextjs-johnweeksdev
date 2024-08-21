204bd14ac0a9846cc899afe9d6a70778bb810039
gottajsx <gottajsx@gmail.com>
2024-08-21 23:29:42 +0200

    Add files via upload

--- app/components/auth/Register.tsx
+++ app/components/auth/Register.tsx
@@ -1,18 +1,22 @@
+import { useGeneralStore } from "@/app/stores/general";
 import TextInput from "../TextInput";
 import { useState } from "react";
 import { ShowErrorObject } from "@/app/types";
 import { useUser } from "@/app/context/user";
-import { useGeneralStore } from "@/app/stores/general";
 import { BiLoaderCircle } from "react-icons/bi";
+import { useRouter } from "next/navigation";
 
-export default function Login() {
+export default function Register() {
     let { setIsLoginOpen } = useGeneralStore();
 
     const contextUser = useUser()
+    const router = useRouter()
 
     const [loading, setLoading] = useState<boolean>(false);
+    const [name, setName] = useState<string | ''>('');
     const [email, setEmail] = useState<string | ''>('');
     const [password, setPassword] = useState<string | ''>('');
+    const [confirmPassword, setConfirmPassword] = useState<string | ''>('');
     const [error, setError] = useState<ShowErrorObject | null>(null)
 
     const showError = (type: string) => {
@@ -26,26 +30,41 @@ export default function Login() {
         setError(null)
         let isError = false
 
-        if (!email) {
+        const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+        if (!name) {
+            setError({ type: 'name', message: 'A Name is required'})
+            isError = true
+        } else if (!email) {
             setError({ type: 'email', message: 'An Email is required'})
+            isError = true
+        } else if (!reg.test(email)) {
+            setError({ type: 'email', message: 'The Email is not valid'})
             isError = true
         } else if (!password) {
             setError({ type: 'password', message: 'A Password is required'})
+            isError = true
+        } else if (password.length < 8) {
+            setError({ type: 'password', message: 'The Password needs to be longer'})
+            isError = true
+        } else if (password != confirmPassword) {
+            setError({ type: 'password', message: 'The Passwords do not match'})
             isError = true
         }
         return isError
     }
 
-    const login = async () => {
+    const register = async () => {
         let isError = validate()
         if (isError) return
         if (!contextUser) return
 
         try {
             setLoading(true)
-            await contextUser.login(email, password)
+            await contextUser.register(name, email, password)
             setLoading(false)
             setIsLoginOpen(false)
+            router.refresh()
         } catch (error) {
             console.log(error)
             setLoading(false)
@@ -56,9 +75,22 @@ export default function Login() {
     return (
         <>
             <div>
-                <h1 className="text-center text-[28px] mb-4 font-bold">Log in</h1>
+                <h1 className="text-center text-[28px] mb-4 font-bold">Register</h1>
 
                 <div className="px-6 pb-2">
+
+                    <TextInput 
+                        string={name}
+                        placeholder="Name"
+                        onUpdate={setName}
+                        inputType="text"
+                        error={showError('name')}
+                    />
+                    
+                </div>
+
+                <div className="px-6 pb-2">
+
                     <TextInput 
                         string={email}
                         placeholder="Email address"
@@ -66,6 +98,7 @@ export default function Login() {
                         inputType="email"
                         error={showError('email')}
                     />
+                    
                 </div>
 
                 <div className="px-6 pb-2">
@@ -78,16 +111,26 @@ export default function Login() {
                     />
                 </div>
 
+                <div className="px-6 pb-2">
+                    <TextInput 
+                        string={confirmPassword}
+                        placeholder="Confirm Password"
+                        onUpdate={setConfirmPassword}
+                        inputType="password"
+                        error={showError('confirmPassword')}
+                    />
+                </div>
+
                 <div className="px-6 pb-2 mt-6">
                     <button 
                         disabled={loading}
-                        onClick={() => login()} 
+                        onClick={() => register()} 
                         className={`
                             flex items-center justify-center w-full text-[17px] font-semibold text-white py-3 rounded-sm
-                            ${(!email || !password) ? 'bg-gray-200' : 'bg-[#F02C56]'}
+                            ${(!name || !email || !password || !confirmPassword) ? 'bg-gray-200' : 'bg-[#F02C56]'}
                         `}
                     >
-                        {loading ? <BiLoaderCircle className="animate-spin" color="#ffffff" size={25} /> : 'Log in'}
+                        {loading ? <BiLoaderCircle className="animate-spin" color="#ffffff" size={25} /> : 'Register'}
                     </button>
                 </div>
             </div>
